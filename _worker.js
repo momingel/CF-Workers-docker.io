ac3ffbc102890255ab248397a7a78725a9ca6a69
momingel <68225848+momingel@users.noreply.github.com>
2024-08-16 21:23:06 +0800

    Update _worker.js
    2024-08-16 21:23:03

--- _worker.js
+++ _worker.js
@@ -21,7 +21,6 @@ function routeByHosts(host) {
 		"ghcr": "ghcr.io",
 		"cloudsmith": "docker.cloudsmith.io",
 		"nvcr": "nvcr.io",
-		"k8s-pkgs": "prod-cdn.packages.k8s.io",
 		
 		// 测试环境
 		"test": "registry-1.docker.io",
